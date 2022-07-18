f3d07f66368f2c87edad3d021bd17ab4d36d0ed9
Alexander45448 <00098818@uca.edu.sv>
2022-07-18 09:00:55 -0600

    first commit

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,31 @@
+
+import { IconButton, Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../views"
+import { AddOutlined } from '@mui/icons-material';
+export const JournalPage = () => {
+  return (
+    <JournalLayout>
+    {/*   <Typography>Irure Lorem laborum adipisicing laboris irure id consequat voluptate fugiat ipsum duis excepteur aliqua in.</Typography>
+     */}
+
+     <NothingSelectedView />
+  {/*   <NoteView />  */}
+
+  <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
+
+    </JournalLayout>
+  )
+}
